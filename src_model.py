fe7021f12e96b1ab81847a0203d30f65aa71d260
Aryan Garg <aryangarg019@gmail.com>
2021-09-18 07:17:13 +0530

    Update model class and module helper comments

--- src_model.py
+++ src_model.py
@@ -1,3 +1,8 @@
+# For all modules that produce an error:
+# In the notebook, before importing, write
+# Replace angular brackets with module name:
+# ! pip install <package-name-as-per-package's-documentation>
+
 import cv2 
 from PIL import Image
 import imutils
@@ -9,8 +14,10 @@ from os import listdir
 from os.path import isfile, join
 import sys
 
+
 print(f"[+]Loading essential packages...")
 print(f"[+]Loaded OpenCV version {cv2.__version__} @ {time.asctime(time.localtime(time.time()))}\n")
+
 
 class preProcessData:
     
@@ -46,6 +53,7 @@ class preProcessData:
         return True
         
     def showSampleImages(self, folderName = "2012-06-05_165931"):
+        ### TODO: Decrease Image size
         print("[+] Logging sample images' details\n---------------------")
         for i in range(1,10,2): 
             img_left = self.allTrainingData[folderName][i-1]
@@ -55,6 +63,8 @@ class preProcessData:
                 imgL = cv2.imread(self.trainDataPath + folderName + "\\RectGrabber\\" + img_left)
                 imgR = cv2.imread(self.trainDataPath + folderName + "\\RectGrabber\\" + img_right)
                 
+                imgL = cv2.resize(imgL, (600,450))
+                imgR = cv2.resize(imgR, (600,450))
                 print(f"{(i//2) + 1}. Image names: {img_left} & {img_right}\n\t\tShape-L: {imgL.shape}     Shape-R: {imgR.shape}\n")
                 
                 if not self.showFrames(["Left frame", "Right frame"], [imgL, imgR]):
@@ -134,14 +144,16 @@ class preProcessData:
                 continue
                 
             img = cv2.resize(img, (600, 450))
-            (rects, weights) = hog.detectMultiScale(img, winStride=(4, 4), padding=(16, 16), scale=1.2)
+            (rects, weights) = hog.detectMultiScale(img, winStride=(4, 4), padding=(8, 8), scale=1.1)
+            
             for (x, y, w, h) in rects:
                 cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
                 cv2.putText(img,'Person', (x,y), font, fontScale,fontColor,lineType)
             
             toRet.append(img)
-            if not self.showFrames(["Bounding Box"], [img]):
-                print(f"Couldn't show pedestrian detected image: {img}")
+                
+            #if not self.showFrames(["Bounding Box"], [img]):
+            #    print(f"Couldn't show pedestrian detected image: {img}")
         
         return toRet
            
@@ -152,9 +164,9 @@ class preProcessData:
         self.allTrainingData = dict()
         if self.readData(self.trainDataPath):
             self.showSampleImages()
-            self.detectedData = []
+            self.detectedData = {}
             for key in self.allTrainingData:
-                self.detectedData.append(self.detectPedestrians(key, self.allTrainingData[key]))
+                self.detectedData[key] = self.detectPedestrians(key, self.allTrainingData[key])
 
 
 class model:
@@ -170,8 +182,164 @@ class model:
         Predicted Trajectory of each pedestrian in scene
                 
     '''
-    pass
     
+    # State Transition stuff ahead:
+    def current_speed(self):
+        # Returns instantaneous v_x, v_y
+        pass
+    
+    
+    def view30Frames(self, key):
+        pass
+    
+    
+    def eq1_positionTransition(self):
+        self.X.append(X[-1] + self.eq2_unicycle())
+        self.eq3()
+    
+    def eq2_unicyle(self):
+        vx , vy = self.current_speed()
+        return 1/self.fps * (vx, vy, 0)
+    
+    
+    def eq3(self):
+        '''
+            phi_t = p( X_t | X_t-1 )
+            p(Xt|Xt−1) = p(Xt−1) ⊗ p(u(vt, ψt))
+        '''
+        pass
+    
+    def discretization_2_grid(self):
+        '''
+            Consider 2x2 cells -> assign probability(from incremental distribution) to each cell
+            Returns: A (the convolution filter mask) 
+        '''
+        pass
+    
+    def eq4(self):
+        '''
+        Model the incremental distribution:
+        
+            p(∆x, ∆y, ∆ψ) ∝  exp(−(∆x−∆tv cos(ψ))^2/2σ_v^2)
+                            .exp(−(∆y−∆tv sin(ψ))^2/2σ_v^2)
+                            .exp(κ∆ψ cos(∆ψ))
+                            .exp(κv cos(∠(∆y, ∆x) − ψ))
+                            
+        Then call self.discretization_2_grid() to get A (the convolution filter mask)
+        Then call eq5 to get phi_t using A and phi_t-1
+        '''
+        pass
+    
+    
+    def eq5(self):
+        '''
+            Φt ∝ A ⊗ Φt−1
+        '''
+        pass
+    
+    
+    def eq6(self):
+        '''
+            Invert distribution in 4 to get A^(-1). 
+            *** For backward prediction steps from XT ***
+        '''
+        pass
+    
+    
+    def eq7(self):
+        '''
+            Carry out forward and backward steps to get each p(Xt | X0, XT) acc. to:
+            p(Xt|X0, XT ) ∝ Φ+t Φ−t
+        '''
+        pass
+    
+    
+    # Location Prior stuff ahead
+    def eq8(self):
+        '''
+            Forward step:
+            Φ+t ∝ p(Xt|Θt)(A ⊗ Φ+t−1)
+            
+            where Θt is the online map
+                  p(X|θi) is the location prior (call eq10 here)
+        '''
+        pass
+    
+    
+    def eq9(self):
+        '''
+            Backward step:
+            Φ−t−1 ∝ p(Xt|Θt)(A^(−1) ⊗ Φ−t)
+            
+            call eq10 from here
+        '''
+        pass
+    
+    
+    def eq10(self):
+        '''
+            Computes Location prior: p(X|θi) acc to:
+            
+            Single Layer Perceptron: 
+            p(X|θi) = 1 / 1 + exp (−a^T . θ_i)
+            
+            where a^T represents weighting params for different features. 
+        '''
+        pass
+    
+    
+    def eq11(self):
+        '''
+            Path Distribution: 
+            p(XM|X0, XT , Θt) = 1 −PI[(M) <-- (t˜=0)](1 − p(Xt˜|X0, XT , Θt))
+        '''
+        pass
+    
+    
+    def eq12(self):
+        '''
+            Cost function to get best weighting params, i.e, a:
+            J(a) = −Summation[ ζi ∈ Xj ] Summation[ Xj ∈ ζi ] log(p(X = Xj |X0, XT , Θt))
+        '''
+        pass
+    
+    # Goal Distribution XT Estimation ahead
+    def eq13(self):
+        '''
+            p(Xt) ::= estimated distribution for the pedestrian’s state at time = t. 
+            p(X−t|X0, XT , Θt) ::= the distribution of that state obtained from prediction at t-1. 
+        
+            [!] After Assumption: Independence
+            Can obtain:
+        
+                pX− (X−t|X0, XT , Θt) ∝ pX− (X0, XT , Θt|X−t)p(X−t)
+        
+        '''
+        pass
+    
+    
+    def eq14(self):
+        '''
+            p(XT ) ∝ Integral of pX− (X0, XT , Θt|Xt).p(Xt).dX
+            
+            [+] Evaluated for the individual particles and the result is used for reweighting
+            Next TODO: Unlikely particles are discarded and randomly resampled at other locations.
+        '''
+        pass
+    
+    
+    def __init__(self, processedData):
+        self.data = processedData
+        self.fps = 30                          # fps
+        self.X = []                            # Contains last t-1 positions & orientations: (x_t, y_t, psi_t
+        self.px0 = 0
+        self.XT = None                         # Latent Variable: Gaussian Mixture Model + Particle Filter
+        self.currentFrame = None
+        for key in self.data:
+            self.pX0 = self.view30Frames(key)  # Watch 1 sec to estimate initial params
+            self.currentFrame = self.data[key][30]
+            self.eq1_positionTransition()
+        
     
 class analytics:
     '''
